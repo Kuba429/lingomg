de28d99d8d20256fd1caf879397bacf09afb6121
Kuba429 <jakubzawadzki429@gmail.com>
2023-04-19 11:23:11 +0200

    check function for fill-in-the-blank question

--- src/routes/exercise/fillBlank/FillBlankExercise.svelte
+++ src/routes/exercise/fillBlank/FillBlankExercise.svelte
@@ -3,10 +3,13 @@
 	import type { fillBlankExercise } from "./utils";
 	export let currentExercise: fillBlankExercise;
 	export let goToNext: () => void;
+	let inputValue: string;
 	// hardcoded for now
-	const checkAnswer = () => true;
+	const checkAnswer = () =>
+		inputValue.trim().toLowerCase() === currentExercise.answer.trim().toLowerCase();
 </script>
 
 <h1>Fill in the blank</h1>
 <h3>{currentExercise.question.split("#BLANK#").join("_".repeat(currentExercise.answer.length))}</h3>
+<input type="text" bind:value={inputValue} />
 <Buttons {goToNext} {checkAnswer} />
