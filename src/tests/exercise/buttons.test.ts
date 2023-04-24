188ac2c47782c0a1e2613635e9d0de649a139698
Kuba429 <jakubzawadzki429@gmail.com>
2023-04-24 16:57:06 +0200

    buttons tests

---
+++ src/tests/exercise/buttons.test.ts
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/svelte";
+import Buttons from "../../routes/exercise/Buttons.svelte";
+import { describe, it, expect } from "vitest";
+import userEvent from "@testing-library/user-event";
+import { sleep } from "$lib/sleep";
+
+describe("buttons component", () => {
+	const props = { goToNext: () => {}, checkAnswer: () => true, currentExerciseAnswer: "answer" };
+	it("mounts", () => {
+		const { container } = render(Buttons, { props });
+		const el = container.querySelector(".buttons");
+		expect(el).toBeInTheDocument();
+	});
+	it("has 'wrong' class when answer in wrong", async () => {
+		const { container } = render(Buttons, { ...props, checkAnswer: () => false });
+		const el = container.querySelector(".buttons");
+		const doneButton = screen.getByText("Done");
+
+		expect(el).not.toHaveClass("wrong");
+		await userEvent.click(doneButton);
+		expect(el).toHaveClass("wrong");
+
+		// back to idle when "Next" is clicked
+		const nextButton = screen.getByText("Next");
+		expect(nextButton).toBeInTheDocument();
+	});
+	it("has 'correct' class when answer in correct", async () => {
+		const { container } = render(Buttons, { props });
+		const el = container.querySelector(".buttons");
+		const doneButton = screen.getByText("Done");
+
+		expect(el).not.toHaveClass("correct");
+		await userEvent.click(doneButton);
+		expect(el).toHaveClass("correct");
+
+		const nextButton = screen.getByText("Next");
+		expect(nextButton).toBeInTheDocument();
+	});
+});
